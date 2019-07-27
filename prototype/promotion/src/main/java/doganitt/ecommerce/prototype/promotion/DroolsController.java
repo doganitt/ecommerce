b0192b240cbd5de0f4a7634e74dfb37bb15c7285
solsin <charles@plgrim.com>
2019-07-27 15:07:25 +0900

    drools controller initial import

---
+++ prototype/promotion/src/main/java/doganitt/ecommerce/prototype/promotion/DroolsController.java
@@ -0,0 +1,13 @@
+package doganitt.ecommerce.prototype.promotion;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DroolsController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
