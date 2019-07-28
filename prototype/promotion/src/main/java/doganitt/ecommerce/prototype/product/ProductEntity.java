5d78253bd02b33da72450d4e612b1f90b4f22162
wonjin <wonjin@LAPTOP-EI4G8R41>
2019-07-28 15:58:02 +0900

    ProductEntity 수정

--- prototype/promotion/src/main/java/doganitt/ecommerce/prototype/product/ProductEntity.java
+++ prototype/promotion/src/main/java/doganitt/ecommerce/prototype/product/ProductEntity.java
@@ -4,4 +4,38 @@ public class ProductEntity {
 	String id;
 	String name;
 	int sellingPrice;
+	int discountedPrice;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSellingPrice() {
+		return sellingPrice;
+	}
+
+	public void setSellingPrice(int sellingPrice) {
+		this.sellingPrice = sellingPrice;
+	}
+
+	public int getDiscountedPrice() {
+		return discountedPrice;
+	}
+
+	public void setDiscountedPrice(int discountedPrice) {
+		this.discountedPrice = discountedPrice;
+	}
+	
 }
